f355d0a15582ef3a0f3a88b5d4ead3fecd9845f5
harshal <patilharshal675@gmail.com>
2024-06-11 13:17:51 +0530

    emulated view cap changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Comp1Component, Comp2Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  // encapsulation: ViewEncapsulation.Emulated,
 })
 export class AppComponent {
   title = 'view-encap';
---
+++ src/app/comp1/comp1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Comp2Component } from '../comp2/comp2.component';
+
+@Component({
+  selector: 'app-comp1',
+  standalone: true,
+  imports: [],
+  templateUrl: './comp1.component.html',
+  styleUrl: './comp1.component.css'
+})
+export class Comp1Component {
+
+}
