32b81f33212d19b7be3b373ff15e5181166c9856
harshal <patilharshal675@gmail.com>
2024-06-11 13:36:40 +0530

    use of ngTemplate and ngTemplateOutlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Comp1Component, Comp2Component],
+  imports: [RouterOutlet, Comp1Component, Comp2Component, NgTemplateOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   encapsulation: ViewEncapsulation.None,
